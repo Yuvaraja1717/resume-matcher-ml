cdb78b43b55b8ee390d59d88a13783134ba72dc7
Yuvaraja1717 <yuvarajrsyuvi01@gmail.com>
2026-04-29 14:02:33 +0530

    Reorganize imports and retain match_resumes function

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.linear_model import LogisticRegression
 
 
 # ---------- TEXT SIMILARITY ----------
@@ -16,17 +15,3 @@ def match_resumes(resumes, job_description):
     scores = [float(score[0]) for score in similarity]
 
     return scores
-
-
-# ---------- LOGISTIC REGRESSION ----------
-def train_logistic_model(features, labels):
-
-    model = LogisticRegression()
-    model.fit(features, labels)
-
-    return model
-
-
-def predict_logistic(model, features):
-
-    return model.predict(features)
